c9c7d018c4634aeb9f61d0793a61a2a9e2c8aabc
Urahara-Kenpachi <is.haqomotosho@gmail.com>
2020-06-30 01:38:16 +1300

    SmackApp

---
+++ app/src/main/java/Constant/Constants.kt
@@ -0,0 +1,4 @@
+package Constant
+
+const val BASE_URL = "https://smackchattychatchatt.herokuapp.com/v1/"
+const val URL_REGISTER ="${BASE_URL}account/register"
--- app/src/main/java/Controller/MainActivity.kt
+++ app/src/main/java/Controller/MainActivity.kt
@@ -1,20 +1,13 @@
-package com.example.smackapp
+package Controller
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.smackapp.R
 
 class MainActivity : AppCompatActivity() {
 
